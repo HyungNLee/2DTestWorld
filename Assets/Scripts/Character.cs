add859c53b29af469eefff2192860f239c85f983
Hyung Lee <hyungnaelee@gmail.com>
2018-10-18 20:46:49 -0700

    Initial commit. Add movement script to player to move with WASD keys. Add 'character' script that 'player' script will inherit from.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+  // Movement speed of character
+  // SerializeField lets the class be public but show up in editor.
+  [SerializeField]
+  protected float speed;
+
+  // Direction of character.
+  // 'Protected' means everything that inhertits from this script can access it.
+  protected Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+  // Made this 'virtual' so the script that inherits from this class can override this Update() method.
+	protected virtual void Update () {
+		Move();
+	}
+
+  // Moves the player according to input from GetInput().
+  public void Move()
+  {
+    transform.Translate(direction * speed * Time.deltaTime);
+  }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	// Use this for initialization
+	void Start () 
+  {
+		speed = 5f;
+	}
+	
+	// Update is called once per frame
+  // Wrote 'override' so this class can override the Update() in the character script.
+	protected override void Update () 
+  {
+		GetInput();
+
+    // base means to access the script that this player script inherits from. In this case, the character script.
+    base.Update();
+	}
+
+  private void GetInput()
+  {
+    direction = Vector2.zero;
+    
+    if (Input.GetKey(KeyCode.W)) {
+      direction += Vector2.up;
+    }
+    if (Input.GetKey(KeyCode.A)) {
+      direction += Vector2.left;
+    }
+    if (Input.GetKey(KeyCode.S)) {
+      direction += Vector2.down;
+    }
+    if (Input.GetKey(KeyCode.D)) {
+      direction += Vector2.right;
+    }
+  }
+}
